168715c99ff01eaf9df2ed8c7c77888314c4ceec
Александр Жирков <magnus134545@yandex.ru>
2023-05-06 16:02:47 +0300

    second

---
+++ README.md
@@ -0,0 +1,150 @@
+Username: "admin@ya.ru". Password: "admin1".
+1. GET(user) - http://localhost:3005/api/users
+
+ ![GETusers](https://user-images.githubusercontent.com/98220684/205493606-3310c780-c20c-472d-b672-fef05137f8cb.png)
+ 
+ Данные пользователей, 200
+ 
+ 2. POST(user) - http://localhost:3005/api/users
+ 
+ {
+    "name": "Александр",
+    "surname": "Жирков",
+    "patronomyc": "Дмитриевич",
+    "email": "magnus134545@yandex.ru",
+    "password": "qwerty12345"
+}
+
+![POSTusers](https://user-images.githubusercontent.com/98220684/205493647-795be08f-485f-4635-af4f-93233c8b23ba.png)
+
+Пользователь создан, 201
+
+3. PUT(user) - http://localhost:3005/api/users/0
+
+{
+    "name": "Матвей"
+}
+
+![PUTusers](https://user-images.githubusercontent.com/98220684/205493676-7a7d1621-0e9e-4735-9412-cab2d6898dfb.png)
+
+Пользователь отредактирован, 200
+
+4. DELETE(user) - http://localhost:3005/api/users/0
+
+![DELETEusers](https://user-images.githubusercontent.com/98220684/205493694-5e1b11e5-7dc2-43d5-b47d-d50e7a3deed3.png)
+
+Пользователь успешно удалён, 200
+
+5. GET(music) -  http://localhost:3005/api/musics
+
+![GETmusic](https://user-images.githubusercontent.com/98220684/205493779-e3d24162-8b18-448c-b53a-0d23b81354e0.png)
+
+Данные трека, 200
+
+6. POST(music) - http://localhost:3005/api/musics
+
+{
+    "title": "After Dark",
+    "executor": "Mr.Kitty",
+    "genre": "Альтернативная музыка/Инди",
+    "duration": "4:17",
+    "rating": "12/10"
+}
+
+![POSTmusic](https://user-images.githubusercontent.com/98220684/205493794-1a7b5876-a40f-45d8-bbd5-c6b7c09822e9.png)
+
+Трек добавлен, 201
+
+7. PUT(music) - http://localhost:3005/api/musics/0
+
+{
+    "title":"123"
+}
+
+![PUTmusic](https://user-images.githubusercontent.com/98220684/205493881-63effecd-907f-443b-b4b7-4d30b7567230.png)
+
+Трек отредактирован, 200
+
+8. DELETE(music) - http://localhost:3005/api/musics/0
+
+![DELETEmusic](https://user-images.githubusercontent.com/98220684/205493888-2541f582-1487-4599-8b70-3b22d6eb2102.png)
+
+Трек успешно удалён, 200
+
+9. GET(book) - http://localhost:3005/api/books
+
+![GETbooks](https://user-images.githubusercontent.com/98220684/205609626-ebaff5aa-bd06-4a71-b1d8-4c2ac9396061.png)
+
+Данные о книге, 200
+
+10. POST(book) - http://localhost:3005/api/books
+
+{
+    "title": "title",
+    "author": "author",
+    "genre": "genre",
+    "numofpages": "numofpages",
+    "year": "year"
+}
+
+![POSTbook](https://user-images.githubusercontent.com/98220684/205610048-6dc7d1ac-98b5-4204-b1e9-7258e65e01df.png)
+
+Книга добавлена, 201
+
+11. PUT(book) - http://localhost:3005/api/books/0
+
+{
+    "title":"title1"
+}
+
+![PUTbook](https://user-images.githubusercontent.com/98220684/205610327-268295a1-90b2-4729-9c4c-6f4ff3543d05.png)
+
+Книга отредактирована, 200
+
+12. DELETE(book) - http://localhost:3005/api/books/0
+
+![DELETEbook](https://user-images.githubusercontent.com/98220684/205610426-318af7e4-f3f9-460d-803e-6f34803fc9fc.png)
+
+Книга успешно удалена, 200
+
+13. GET(car) - http://localhost:3005/api/cars
+
+![GETcars](https://user-images.githubusercontent.com/98220684/205610860-212a07fc-bab2-4101-9972-0171124b7d88.png)
+
+
+Данные о машине, 200
+
+14. POST(car) - http://localhost:3005/api/cars
+
+{
+    "name": "name",
+    "brand": "brand",
+    "color": "color",
+    "speed": "speed",
+    "year": "year"
+}
+
+![POSTcar](https://user-images.githubusercontent.com/98220684/205610883-de85d614-10cb-48a5-9ec8-4a18f78c86a3.png)
+
+
+Машина добавлена, 201
+
+15. PUT(car) - http://localhost:3005/api/cars/0
+
+{
+    "brand": "brand1"
+}
+
+![PUTcar](https://user-images.githubusercontent.com/98220684/205610903-4b15ddf5-c093-4b6f-b0e7-2b86bd9f8b89.png)
+
+Машина отредактирована, 200
+
+16. DELETE(car) - http://localhost:3005/api/cars/0
+
+![DELETEcar](https://user-images.githubusercontent.com/98220684/205610923-5f2291c1-2cd6-4163-bde3-e0981c086397.png)
+
+Машина успешно удалена, 200
+
+
+
+
---
+++ main.py
@@ -0,0 +1,467 @@
+from flask import *
+from jinja2 import Template
+
+
+app = Flask(__name__)
+
+app_context = app.app_context()
+app_context.push()
+
+app = Flask(__name__, template_folder="templates") #Позже добавить jinja
+app = Flask(__name__, static_folder="static")
+t = Template("Name:{{mainTitle}}", "Summ:{{a+b}}")
+t.render(name='Основная программа', a='2', b='3')
+
+
+#Создание массива для хранения юзеров
+users = []
+#Создание массива для хранения музыки
+musics = []
+#Создание массива для хранение книг
+books = []
+#Создание массива для хранения машин
+cars = []
+
+
+
+
+#Создание класса, который будет хранить записи юзеров
+class User(object):
+    def __init__(self, name, surname, patronomyc, email, password):
+        self.name = name
+        self.surname = surname
+        self.patronomyc = patronomyc
+        self.email = email
+        self.password = password
+
+#Создание класса, который будет хранить записи музыки
+class Music(object):
+    def __init__(self, title, executor, genre, duration, rating):
+        self.title = title
+        self.executor = executor
+        self.genre = genre
+        self.duration = duration
+        self.rating = rating
+
+#Создание класса, который будет хранить записи книг
+class Book(object):
+    def __init__(self, title, author, genre, numofpages, year):
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.numofpages = numofpages
+        self.year = year
+
+#Создание класса, который будет хранить записи машин
+class Car(object):
+    def __init__(self, name, brand, color, speed, year):
+        self.name = name
+        self.brand = brand
+        self.color = color
+        self.speed = speed
+        self.year = year
+
+def check_credentials(username, password):
+    if username == 'admin' and password == 'admin1':
+        return True
+    else:
+        return False
+
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin@ya.ru' and password == 'admin1':
+            return redirect('/api')
+        else:
+            return render_template('auth.html', error=True)
+    return render_template('auth.html', error=False)
+
+@app.route('/')
+def home():
+    return render_template('auth.html')
+
+
+@app.route('/api', methods=['GET'])
+def mainpage():
+    return render_template('index.html') 
+
+#Метод получения всех юзеров, пытался сделать для каждого отдельно дополнительно, но что-то пошло не так
+@app.route('/api/users/', methods=['GET'])
+def get_users():
+
+    user = ''
+    u = ''
+    num = 0
+    for i in users:
+        num += 1
+        user = "<b>" + "Имя: " + "</b>" + i.name + ", " + "<br>" + "<b>" + "Фамилия: " + "</b>" + i.surname + ", " + "<br>" + "<b>" + "Отчество: " + "</b>" + i.patronomyc + ", " + "<br>" + "<b>" + "Адрес электронной почты: " + "</b>" + i.email + ", " + "<br>" + "<b>" + "Пароль: " + "</b>" +  i.password
+        u += "<div class='block'>" + "<b>" + "<u>" + f"Пользователь {num}: " + "</u>" + "</b>" + "<br>" + user + "<br>" + "<br>" + "</div>" 
+    return u, 200
+    
+
+#Не работает, хз почему, но это и не нужно, для себя пытался сделать
+@app.route('/api/users/<string:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = list(filter(lambda u: u['id'] == user_id, users))
+    if user == None:
+        abort(404)
+    return jsonify({'user': user[0]})
+
+#Метод получения всей музыки
+@app.route('/api/musics/', methods=['GET'])
+def get_musics():
+    music = ''
+    u = ''
+    num = 0
+    for i in musics:
+        num += 1
+        music = "<b>" + "Название трека: " + "</b>" + i.title + ", " + "<br>" + "<b>" + "Исполнитель: " + "</b>" + i.executor + ", " + "<br>" + "<b>" + "Жанр: " + "</b>" + i.genre + ", " + "<br>" + "<b>" + "Длительность: " + "</b>" + i.duration + ", " + "<br>" + "<b>" + "Рейтинг: " + "</b>" +  i.rating
+        u += "<b>" + "<u>" + f"Трек {num}: " + "</u>" + "</b>" + "<br>" + music + "<br>" + "<br>"
+    return u, 200
+
+#Метод получения всех книг
+@app.route('/api/books/', methods=['GET'])
+def get_books():
+    book = ''
+    u = ''
+    num = 0
+    for i in books:
+        num += 1
+        book = "<b>" + "Название книги: " + "</b>" + i.title + ", " + "<br>" + "<b>" + "Автор книги: " + "</b>" + i.author + ", " + "<br>" + "<b>" + "Жанр: " + "</b>" + i.genre + ", " + "<br>" + "<b>" + "Количество страниц: " + "</b>" + i.numofpages + ", " + "<br>" + "<b>" + "Год издания: " + "</b>" +  i.year
+        u += "<b>" + "<u>" + f"Книга {num}: " + "</u>" + "</b>" + "<br>" + book + "<br>" + "<br>"
+    return u, 200
+
+#Метод получения всех машин
+@app.route('/api/cars/', methods=['GET'])
+def get_cars():
+    car = ''
+    u = ''
+    num = 0
+    for i in cars:
+        num += 1
+        car = "<b>" + "Модель машины: " + "</b>" + i.name + ", " + "<br>" + "<b>" + "Марка машины: " + "</b>" + i.brand + ", " + "<br>" + "<b>" + "Цвет машины: " + "</b>" + i.color + ", " + "<br>" + "<b>" + "Максимальная скорость машины: " + "</b>" + i.speed + ", " + "<br>" + "<b>" + "Год выпуска машины: " + "</b>" +  i.year
+        u += "<b>" + "<u>" + f"Машина {num}: " + "</u>" + "</b>" + "<br>" + car + "<br>" + "<br>"
+    return u, 200
+
+
+
+#Метод создания новых юзеров
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    projectpath = request.form['regform']
+    rqst = request.get_json()
+    name = None
+    surname = None
+    patronomyc = None
+    email = None
+    password = None
+
+    if rqst:
+        if 'name' in rqst:
+            name = rqst['name']
+        else:
+            return 'Не обнаружено поле "name"', 403
+        if 'surname' in rqst:
+            surname = rqst['surname']
+        else:
+            return 'Не обнаружено поле "surname"', 403
+        if 'patronomyc' in rqst:
+            patronomyc = rqst['patronomyc']
+        else:
+            return 'Не обнаружено поле "patronomyc"', 403
+        if 'email' in rqst:
+            email = rqst['email']
+        else:
+            return 'Не обнаружено поле "email"', 403
+        if 'password' in rqst:
+            password = rqst['password']
+        else:
+            return 'Не обнаружено поле "password"', 403
+    else:
+        return 'Данных недостаточно', 403
+    if name and surname and patronomyc and email and password:
+        user = User(name, surname, patronomyc, email, password)
+        users.append(user)
+        return 'Пользователь создан', 201
+
+#Первая попытка в метод пут, не получилось, решил переделать по другому
+'''
+@app.route('/api/put/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = list(filter(lambda u: u['id'] == user_id, users))
+    if len(user) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'name' in request.json and type(request.json['name']) != str:
+        abort(400)
+    if 'surname' in request.json and type(request.json['surname']) != str:
+        abort(400)
+    if 'patronomyc' in request.json and type(request.json['patronomyc']) != str:
+        abort(400)
+    if 'email' in request.json and type(request.json['email']) != str:
+        abort(400)
+    if 'password' in request.json and type(request.json['password']) != str:
+        abort(400)
+    user[0]['name'] = request.json.get('name', user[0]['name'])
+    user[0]['surname'] = request.json.get('surname', user[0]['surname'])
+    user[0]['patronomyc'] = request.json.get('patronomyc', user[0]['patronomyc'])
+    user[0]['email'] = request.json.get('email', user[0]['email'])
+    user[0]['password'] = request.json.get('password', user[0]['password'])
+    return jsonify({'user': user[0]})
+'''
+
+#Метод создания новой музыки
+@app.route('/api/musics', methods=['POST'])
+def create_music():
+    rqst = request.get_json()
+    title = None
+    executor = None
+    genre = None
+    duration = None
+    rating = None
+
+    if rqst:
+        if 'title' in rqst:
+            title = rqst['title']
+        else:
+            return 'Не обнаружено поле "title"', 403
+        if 'executor' in rqst:
+            executor = rqst['executor']
+        else:
+            return 'Не обнаружено поле "executor"', 403
+        if 'genre' in rqst:
+            genre = rqst['genre']
+        else:
+            return 'Не обнаружено поле "genre"', 403
+        if 'duration' in rqst:
+            duration = rqst['duration']
+        else:
+            return 'Не обнаружено поле "duration"', 403
+        if 'rating' in rqst:
+            rating = rqst['rating']
+        else:
+            return 'Не обнаружено поле "rating"', 403
+    else:
+        return 'Данных недостаточно', 403
+    if title and executor and genre and duration and rating:
+        music = Music(title, executor, genre, duration, rating)
+        musics.append(music)
+        return 'Трек добавлен', 201
+
+#Метод добавления новых книг
+@app.route('/api/books', methods=['POST'])
+def create_book():
+    rqst = request.get_json()
+    title = None
+    author = None
+    genre = None
+    numofpages = None
+    year = None
+
+    if rqst:
+        if 'title' in rqst:
+            title = rqst['title']
+        else:
+            return 'Не обнаружено поле "title"', 403
+        if 'author' in rqst:
+            author = rqst['author']
+        else:
+            return 'Не обнаружено поле "author"', 403
+        if 'genre' in rqst:
+            genre = rqst['genre']
+        else:
+            return 'Не обнаружено поле "genre"', 403
+        if 'numofpages' in rqst:
+            numofpages = rqst['numofpages']
+        else:
+            return 'Не обнаружено поле "numofpages"', 403
+        if 'year' in rqst:
+            year = rqst['year']
+        else:
+            return 'Не обнаружено поле "year"', 403
+    else:
+        return 'Данных недостаточно', 403
+    if title and author and genre and numofpages and year:
+        book = Book(title, author, genre, numofpages, year)
+        books.append(book)
+        return 'Книга добавлена', 201
+
+#Метод добавления новых машин
+@app.route('/api/cars', methods=['POST'])
+def create_car():
+    rqst = request.get_json()
+    name = None
+    brand = None
+    color = None
+    speed = None
+    year = None
+
+    if rqst:
+        if 'name' in rqst:
+            name = rqst['name']
+        else:
+            return 'Не обнаружено поле "name"', 403
+        if 'brand' in rqst:
+            brand = rqst['brand']
+        else:
+            return 'Не обнаружено поле "brand"', 403
+        if 'color' in rqst:
+            color = rqst['color']
+        else:
+            return 'Не обнаружено поле "color"', 403
+        if 'speed' in rqst:
+            speed = rqst['speed']
+        else:
+            return 'Не обнаружено поле "speed"', 403
+        if 'year' in rqst:
+            year = rqst['year']
+        else:
+            return 'Не обнаружено поле "year"', 403
+    else:
+        return 'Данных недостаточно', 403
+    if name and brand and color and speed and year:
+        car = Car(name, brand, color, speed, year)
+        cars.append(car)
+        return 'Машина добавлена', 201
+
+#Метод для редактирования юзеров
+@app.route('/api/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    if len(users) == 0 or len(users) == None:
+        abort(404)
+    rqst = request.get_json()
+    if rqst:
+        if 'name' in rqst:
+            users[user_id].name = rqst['name']
+        if 'surname' in rqst:
+            users[user_id].surname = rqst['surname']
+        if 'patronomyc' in rqst:
+            users[user_id].patronomyc = rqst['patronomyc']
+        if 'email' in rqst:
+            users[user_id].email = rqst['email']
+        if 'password' in rqst:
+            users[user_id].password = rqst['password']
+    else:
+        return "Недостаточно данных", 403
+    return "Пользователь успешно отредактирован", 200
+
+#Метод для редактирования музыки
+@app.route('/api/musics/<int:music_id>', methods=['PUT'])
+def update_music(music_id):
+    if len(musics) == 0 or len(musics) == None:
+        abort(404)
+    rqst = request.get_json()
+    if rqst:
+        if 'title' in rqst:
+            musics[music_id].title = rqst['title']
+        if 'executor' in rqst:
+            musics[music_id].executor = rqst['executor']
+        if 'genre' in rqst:
+            musics[music_id].genre = rqst['genre']
+        if 'duration' in rqst:
+            musics[music_id].duration = rqst['duration']
+        if 'rating' in rqst:
+            musics[music_id].rating = rqst['rating']
+    else:
+        return "Недостаточно данных", 403
+    return "Трек успешно отредактирован", 200
+
+#Метод для редактирования книг
+@app.route('/api/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    if len(books) == 0 or len(books) == None:
+        abort(404)
+    rqst = request.get_json()
+    if rqst:
+        if 'title' in rqst:
+            books[book_id].title = rqst['title']
+        if 'author' in rqst:
+            books[book_id].author = rqst['author']
+        if 'genre' in rqst:
+            books[book_id].genre = rqst['genre']
+        if 'numofpages' in rqst:
+            books[book_id].numofpages = rqst['numofpages']
+        if 'year' in rqst:
+            books[book_id].year = rqst['year']
+    else:
+        return "Недостаточно данных", 403
+    return "Книга успешно отредактирована", 200
+
+#Метод для редактирования машин
+@app.route('/api/cars/<int:car_id>', methods=['PUT'])
+def update_car(car_id):
+    if len(cars) == 0 or len(cars) == None:
+        abort(404)
+    rqst = request.get_json()
+    if rqst:
+        if 'name' in rqst:
+            cars[car_id].name = rqst['name']
+        if 'brand' in rqst:
+            cars[car_id].brand = rqst['brand']
+        if 'color' in rqst:
+            cars[car_id].color = rqst['color']
+        if 'speed' in rqst:
+            cars[car_id].speed = rqst['speed']
+        if 'year' in rqst:
+            cars[car_id].year = rqst['year']
+    else:
+        return "Недостаточно данных", 403
+    return "Машина успешно отредактирована", 200
+
+#Тоже что-то не работает, ошибка, с которой я пытался разобраться очень много времени, пришлось переделывать
+'''
+@app.route('/api/delete/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = list(filter(lambda u: u['name'] == user_id, users))
+    if len(user) == 0:
+        abort(404)
+    users.remove(user[0])
+    return jsonify({'result': True})
+'''
+
+#Метод для удаления юзеров по айдишнику
+@app.route('/api/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    if len(users) == 0 or len(users) == None:
+        abort(404)
+    users.pop(user_id)
+    return "Пользователь успешно удален", 200
+
+#Метод для удаления треков по айдишнику
+@app.route('/api/musics/<int:music_id>', methods=['DELETE'])
+def delete_music(music_id):
+    if len(musics) == 0 or len(musics) == None:
+        abort(404)
+    musics.pop(music_id)
+    return "Трек успешно удален", 200
+
+#Метод для удаления книг по айдишнику
+@app.route('/api/books/<int:book_id>', methods=['DELETE'])
+def delete_book(book_id):
+    if len(books) == 0 or len(books) == None:
+        abort(404)
+    books.pop(book_id)
+    return "Книга успешно удалена", 200
+
+#Метод для удаления машин по айдишнику
+@app.route('/api/cars/<int:car_id>', methods=['DELETE'])
+def delete_car(car_id):
+    if len(cars) == 0 or len(cars) == None:
+        abort(404)
+    cars.pop(car_id)
+    return "Машина успешно удалена", 200
+
+#Создание текста для 404 ошибки
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'Ошибка': 'Не найдено'}), 404)
+
+#Ну и стандартная строчка для запуска кода
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=3005)
+
+
+
